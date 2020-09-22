c30fb1110db3680c0ef055ccb40ee87032d52a94
bhavesh muchhadiya <bhaveshmuchhadiya1999.com>
2020-09-22 10:44:53 +0530

    master page complate

--- ELMS/settings.py
+++ ELMS/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ELMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    'static',
+]
