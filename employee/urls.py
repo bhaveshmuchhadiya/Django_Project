fb45f6dbed809c4059d51b9d16b0507574b3fad6
bhavesh muchhadiya <bhaveshmuchhadiya1999.com>
2020-10-11 12:31:41 +0530

    login complate at 11/10/2020

--- employee/urls.py
+++ employee/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.index, name="index"),
+    path('index', views.index, name="index"),
+    path('home', views.home, name="home"),
     path('login', views.login, name="login"),
+    path('process', views.process, name="process"),
+    path('logout', views.logout, name="logout"),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 def login(request):
     return render(request, "login.html")
+def home(request):
+    if request.session.has_key('username'):
+        return render(request, 'home.html')
+    else:
+        return render(request, 'login.html')
+def process(request):
+    user = request.POST['txt1']
+    request.session['username'] = user
+    response = redirect(home)
+    return response
+def logout(request):
+    del request.session['username']
+    response = redirect(login)
+    return response
